c335df5d3cd639204cef03d29c5a2a1049250fd9
marecl <fotelpl@gmail.com>
2020-03-15 15:39:14 +0000

    Added license

--- src/HPDL1414.cpp
+++ src/HPDL1414.cpp
@@ -1,5 +1,24 @@
 #include "HPDL1414.h"
 
+/*
+    HPDL1414 Arduino Library
+
+    Copyright (C) 2020  Marek Ledworowski (@marecl)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 HPDL1414::HPDL1414(const byte* _data, const byte* _address,
                    const byte* _wren, const byte _count)
   : dp(_data), ap(_address), wr(_wren), c(_count), maxcap(_count * 4) {};
